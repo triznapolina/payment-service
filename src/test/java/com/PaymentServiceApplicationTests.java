852f8d78334707168280aad3ef12a2429abc6238
trpoliSo <tryznapalina@gmail.com>
2025-12-22 18:24:18 +0300

    init commit

---
+++ src/test/java/com/PaymentServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
